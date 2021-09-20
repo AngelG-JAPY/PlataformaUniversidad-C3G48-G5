d30cc79a0758c00e1b576c5a282880e55def0fd4
JS1305 <sebasriveros1305@gmail.com>
2021-09-20 16:33:50 -0500

    se establece un fondo

--- plataforma-universidad/src/views/Perfil.vue
+++ plataforma-universidad/src/views/Perfil.vue
@@ -1,11 +1,33 @@
 <template>
-   <h1></h1>
+<v-contaier>
+    <span class="bg"></span>
+  <!--<div class="text-center">
+    <v-btn class="ma-2" outlined color="indigo"> Outlined Button </v-btn>
+    <v-btn class="ma-2" outlined fab color="teal">
+      <v-icon>mdi-format-list-bulleted-square</v-icon>
+    </v-btn>
+    <v-btn class="ma-2" outlined large fab color="indigo">
+      <v-icon>mdi-pencil</v-icon>
+    </v-btn>
+  </div>-->
+ </v-contaier>
 </template>
 <script>
-export default {
-    
+  export default {
+      
 }
 </script>
 <style scoped>
+.bg {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
 
+  background: url("/Users/macbook/Documents/MINTIC/Ciclo3/PlataformaUniversidad-C3G48-G5/plataforma-universidad/fondo2.jpg")
+    no-repeat center center;
+  background-size: cover;
+  background-color: gray;  
+}
 </style>
