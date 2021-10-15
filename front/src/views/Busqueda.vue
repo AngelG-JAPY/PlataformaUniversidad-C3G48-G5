9f161fef9fff9e09a4d75e5920d82dd1e04104f0
DiDi7T <dtnirvana@gmail.com>
2021-10-14 21:29:08 -0500

    busqueda con vista

--- front/src/views/Busqueda.vue
+++ front/src/views/Busqueda.vue
@@ -14,9 +14,8 @@
     </v-card>
 
     <!-- Acá inicia el formulario de busqueda -->
-    <v-row class="mt-10" justify="left">
-      <v-col cols="12" sm="12" lg="3">
-        <v-form ref="form" v-model="valid" lazy-validation>
+
+    <!-- <v-form ref="form" v-model="valid" lazy-validation>
 
           <v-select v-model="select" :items="pais" label="Pais"></v-select>
 
@@ -37,27 +36,66 @@
           <v-btn color="success" class="mr-4"> Buscar </v-btn>
 
           <v-btn color="error" class="mr-4"> Borrar Filtros </v-btn>
-        </v-form>
-      </v-col>
-      <v-col cols="12" sm="12" lg="6">
-        <!-- <v-row justify="center"> -->
-        <busquedalist />
+        </v-form> -->
 
-        <!-- </v-row> -->
+    <v-row class="mt-10" justify="left">
+      <v-col cols="12" sm="12" lg="3">
+        <div class="busquedas">
+          <v-row>
+            <v-col
+              v-for="busqueda in busquedas"
+              :key="busqueda.code"
+              md="4"
+              sm="6"
+            >
+              <v-card class="mx-auto" max-width="300">
+                <v-img
+                  class="black--text align-end"
+                  height="200px"
+                  :src="
+                    busqueda.imageUrl == undefined
+                      ? 'https://www.webempresa.com/foro/wp-content/uploads/wpforo/attachments/3200/318277=80538-Sin_imagen_disponible.jpg'
+                      : busqueda.imageURL
+                  "
+                >
+                  <v-card-title class="black--text">{{
+                    busqueda.nombre
+                  }}</v-card-title>
+                </v-img>
+
+                <v-card-subtitle class="pb-0">
+                  {{ busqueda.pais }}</v-card-subtitle
+                >
+
+                <v-card-text class="text--primary">
+                  <div>{{ busqueda.ciudad }}</div>
+                </v-card-text>
+
+                <v-card-actions>
+                  <v-btn color="orange" text @click="leermas(busqueda.pagina)">
+                    Saber mas
+                  </v-btn>
+                </v-card-actions>
+              </v-card>
+            </v-col>
+          </v-row>
+        </div>
       </v-col>
     </v-row>
   </v-main>
 </template>
 
 <script>
-import busquedalist from "../components/busquedaList.vue"
-// import { getAllUniversidades } from "../Services"
+import { getAllUniversidades } from "../Services/Busqueda.service";
+// import busquedalist from "../components/busquedaList.vue";
 export default {
-  components:{
-    busquedalist,
-  },
+  // components: {
+  //   busquedalist,
+  // },
+
   data() {
     return {
+      busquedas: [],
       // pais: ["Colombia"],
 
       // departamento: [
@@ -78,7 +116,7 @@ export default {
 
       imagenes: [
         {
-          src: require("../assets/slbt3.jpg")
+          src: require("../assets/slbt3.jpg"),
         },
         {
           src: "https://cdn.vuetifyjs.com/images/carousel/sky.jpg",
@@ -92,6 +130,24 @@ export default {
       ],
     };
   },
+  mounted() {
+    getAllUniversidades()
+      .then((response) => {
+        this.busquedas = response.data;
+      })
+      .catch((err) => console.error(err))
+  },
+  methods: {
+    leermas(pagina) {
+      this.$router.push(`${pagina}`);
+    },
+    // getAllUniversidades()
+    //   .then((response) => {
+    //     this.busqueda = response.data;
+    //   })
+    //   .catch((err) => console.error(err)),
+  },
+
 };
 </script>
 <style scoped>
