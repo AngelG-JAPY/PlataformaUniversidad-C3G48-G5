11dfac239eab571ecae255ae68105729889c39e7
Maria <mariasirleytangarifegil@gmailcom>
2021-10-15 21:25:30 -0500

    solucionando heroku

--- back/controllers/mailController.js
+++ back/controllers/mailController.js
@@ -1,48 +1,42 @@
-const { restart } = require("nodemon");
-
 module.exports = class mailController {
-    static sendEmail(req, res) {
-        try {
-            const mensaje = req.body;
-            console.log("enviar mensaje: ", mensaje);
-            if (mensaje.msg == undefined || mensaje.msg === "") {
-                res.status(400).json({ mensaje: "No envie mensajes vacios " });
-            } else {
-                var nodemailer = require("nodemailer")
+  static sendEmail(req, res) {
+    try {
+      const mensaje = req.body;
+      console.log("enviar mensaje: ", mensaje);
+      if (mensaje.msg == undefined || mensaje.msg === "") {
+        res.status(400).json({ mensaje: "No envie mensajes vacios " });
+      } else {
+        var nodemailer = require("nodemailer");
 
-                    var transporter = nodemailer.createTransport({
-                        host: "smtp.gmail.com",
-                        post: 587,
-                        secure: false,
-                        auth: {
-                            user: "universidadg48g5@gmail.com",
-                            pass: "kuobcsoodfhzckel",
+        var transporter = nodemailer.createTransport({
+          host: "smtp.gmail.com",
+          post: 587,
+          secure: false,
+          auth: {
+            user: "universidadg48g5@gmail.com",
+            pass: "kuobcsoodfhzckel",
+          },
+        });
+        var mailOptions = {
+          from: "universidad",
+          to: "universidadg48g5@gmail.com",
+          subject: "Pregunta Nueva",
+          text: mensaje.msg,
+        };
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            res.status(500).sen(error.message);
+          } else {
+            console.log("Email enviado.");
+            res.status(200).jsonp(req.body);
+          }
+        });
 
-                        }
-                    });
-                    var mailOptions = {
-                        from: "universidad",
-                        to: "universidadg48g5@gmail.com",
-                        subject: "Pregunta Nueva",
-                        text: mensaje.msg,
-                    }
-                    transporter.sendMail(mailOptions, (error, info) => {
-                        if (error) {
-                            res.status(500).sen(error.message);
-                        } else {
-                            console.log("Email enviado.");
-                            res.status(200).jsonp(req.body);
-                        }
-                    });
-                
-                res.status(200).json();
-            }
-
-        } catch (err) {
-            res.status(404).json({ message: err.message });
-        }
+        res.status(200).json();
+      }
+    } catch (err) {
+      res.status(404).json({ message: err.message });
     }
-}
- //conexion a correo electroinco universidadg48g5@gmail.com
-
-
+  }
+};
+//conexion a correo electroinco universidadg48g5@gmail.com
